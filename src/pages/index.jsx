80a5a81646a478030d00fa7d21e0c21471da56e3
Howard Sun <komugi@gmail.com>
2024-04-21 23:36:46 -0400

    Creating SceneInit for Cameras and angle

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as THREE from 'three';
 import { useEffect } from 'react';
 
+
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center'>
--- src/pages/lib/SceneInit.js
+++ src/pages/lib/SceneInit.js
@@ -1,4 +1,3 @@
-import { render } from 'react-dom';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
