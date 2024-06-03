028e035e045bb3c6bb3178ffb74bf7d386f4edf5
Howard Sun <howardsun@pm.me>
2024-06-02 21:04:41 -0400

    Changed 3js solar system render

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import * as THREE from 'three';
 import { useEffect } from 'react';
 
-
 export default function Home() {
+  let gui;
+
+  const initGui = async () => {
+    const dat = await import('dat.gui');
+    gui = new dat.GUI();
+  };
+
   return (
     <div className='flex flex-col items-center justify-center'>
       <canvas id='myThreeJsCanvas' />
